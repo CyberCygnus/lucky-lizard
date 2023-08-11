e28dd53d4a0143700479444dd9491aa1511208f4
CyberCygnus <132933588+CyberCygnus@users.noreply.github.com>
2023-08-11 15:45:40 +0200

    Add functions 'dealerChoice' and 'determineWinner'.  Add eventListener.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,12 +10,31 @@ const gameRules = {
 let playerScore = 0;
 let dealerScore = 0;
 
-// event listeners
-
 // functions
 function dealerChoice() {
-  const choices = ["🪨", "📃", "✂️", "🦎", "🖖"];
-  return choices[Math.floor(Math.random() * choices.length)];
+  let choices = ["🪨", "📃", "✂️", "🦎", "🖖"];
+  let randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
 }
 
 // game logic
+function determineWinner(player, dealer) {
+  if (player === dealer) {
+    return "draw";
+  }
+  if (gameRules[player].includes(dealer)) {
+    return "player";
+  } else {
+    return "dealer";
+  }
+}
+
+// function playGame(playerChoice) {}
+
+// event listeners
+document.querySelectorAll(".choice").forEach(function (button) {
+  button.addEventListener("click", function (event) {
+    let playerChoice = event.target.innerText;
+    playGame(playerChoice);
+  });
+});
