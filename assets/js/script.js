48f734625a400339ffc93efe75e7863b221c84f1
CyberCygnus <132933588+CyberCygnus@users.noreply.github.com>
2023-08-11 19:42:25 +0200

    Add documentation-style comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,11 @@ let playerScore = 0;
 let dealerScore = 0;
 
 // functions
+
+/**
+ Generates a random choice for the dealer among the available game symbols.
+ The symbol represents the dealer's choice.
+ */
 function dealerChoice() {
   let choices = ["🪨", "📃", "✂️", "🦎", "🖖"];
   let randomIndex = Math.floor(Math.random() * choices.length);
@@ -18,6 +23,10 @@ function dealerChoice() {
 }
 
 // game logic
+
+/**
+ * Determines the winner of the game round based on player and dealer choices.
+ */
 function determineWinner(player, dealer) {
   if (player === dealer) {
     return "draw";
@@ -29,9 +38,17 @@ function determineWinner(player, dealer) {
   }
 }
 
+/**
+ * calculates the winner, updates scores, and displays results.
+ */
 // function playGame(playerChoice) {}
 
 // event listeners
+
+/**
+ * Handles player's choice input by binding click events to choice symbols.
+ * Initiates the game round upon a choice.
+ */
 document.querySelectorAll(".choice").forEach(function (button) {
   button.addEventListener("click", function (event) {
     let playerChoice = event.target.innerText;
